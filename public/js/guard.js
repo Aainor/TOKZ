f41d4d37363570928ec87354a3d4458a2de7ced9
Aainor <orianacasasardone@gmail.com>
2025-12-29 15:54:48 -0300

    Actualización de Más Servicios + animación Guard

---
+++ public/js/guard.js
@@ -0,0 +1,17 @@
+const observerOptions = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.1 
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('appear');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, observerOptions);
+
+    const hiddenElements = document.querySelectorAll('.fade-in');
+    hiddenElements.forEach((el) => observer.observe(el));
